f914c6408c8c5fd683a5d94f3745fee20f723fab
Jeferson Kazuma <jeferson.kazuma@gmail.com>
2018-01-18 21:07:46 -0200

    Adiciona o menu mobile

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,16 +6,26 @@ class Header extends Component {
     super(props)
   
     this.state = {
-      classe: 'bg-color-transparent'
+      classe: 'bg-color-transparent',
+      isMobile: false
     }
+    this.openMenu = this.openMenu.bind(this);
   }
   
   componentDidMount() {
     window.addEventListener('scroll', (event) => {
-      this.setState({
-        classe: window.scrollY === 0 ? 'bg-color-transparent' : 'bg-color-light-dark' 
-      })
+      if (window.innerWidth >= 767) {
+        this.setState({
+          classe: window.scrollY === 0 ? 'bg-color-transparent' : 'bg-color-light-dark'
+        });
+      }
     });
+  }
+
+  openMenu() {
+    this.setState(prevState => ({
+      isMobile: !prevState.isMobile
+    }))
   }
 
   render() {
@@ -23,7 +33,8 @@ class Header extends Component {
       <header className={this.state.classe}>
         <div>
           <a className='logo' href='/' title='Quéops Engenharia'>Quéops Engenharia</a>
-          <nav className='menu'>
+          <i className='fa fa-bars menu-mobile' onClick={this.openMenu}></i>
+          <nav className={`menu ${this.state.isMobile ? 'active' : ''}`}>
             <ul>
               <li>
                 <a href='/' title='Quem somos'>Quem somos</a>
