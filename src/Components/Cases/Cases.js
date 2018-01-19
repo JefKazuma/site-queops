fc9baed3537603e59a4c058534edc006aaac23fd
Jeferson Kazuma <jeferson.kazuma@gmail.com>
2018-01-18 23:25:20 -0200

    Adiciona o component Cases

---
+++ src/Components/Cases/Cases.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Cases.scss';
+
+class Cases extends Component {
+  render() {
+    return (
+      <div id='cases'>
+        
+      </div>
+    );
+  }
+}
+
+export default Cases;
