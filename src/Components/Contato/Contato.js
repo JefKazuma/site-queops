12bcf9c9352523fa42edc93289c5fac142e3c16f
Jeferson Kazuma <jeferson.kazuma@gmail.com>
2018-01-18 23:25:36 -0200

    Adiciona o component Contato

---
+++ src/Components/Contato/Contato.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Contato.scss';
+
+class Contato extends Component {
+  render() {
+    return (
+      <div id='contato'>
+        
+      </div>
+    );
+  }
+}
+
+export default Contato;
