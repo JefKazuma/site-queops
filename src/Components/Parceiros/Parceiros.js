c2f01f094273b6bbeb920d2971e5150aaaddcb82
Jeferson Kazuma <jeferson.kazuma@gmail.com>
2018-01-18 23:25:51 -0200

    Adiciona o component Parceiros

---
+++ src/Components/Parceiros/Parceiros.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Parceiros.scss';
+
+class Parceiros extends Component {
+  render() {
+    return (
+      <div id='parceiros'>
+        
+      </div>
+    );
+  }
+}
+
+export default Parceiros;
